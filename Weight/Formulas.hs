ca5f2ff01c7fbd72d6a4c25eb10888a105d76dc4
DavidM <toad3k@gmail.com>
2013-01-27 05:44:26 -0500

    More tweaks to formulas.
    - Fixed some bugs with low weights.  Probably added a few more in.
    - Stagnancy checks were not correct, they require two workouts.
    - Known bug, if you improve or regress on a workout but don't change
      weight, this program can have trouble determining the rep count you
      intended to use in the future.

--- Weight/Formulas.hs
+++ Weight/Formulas.hs
@@ -3,6 +3,121 @@ module Weight.Formulas(
 ) where
 
 
+import Data.Time
+
+import Weight.Types
+
+data TryThis = TryThis Reps Weight deriving Show
+
+-- DidThis entries are sorted newest to oldest
+data DidThis  = DidThis {
+  toldreps :: Reps,
+  reps :: Reps,
+  weight :: Weight,
+  day :: Day
+} deriving Show
+
+data Progress = Stagnant | Improved | Regressed
+
+
+adjustProfByReps :: Reps -> Proficiency -> Proficiency
+adjustProfByReps r' (Pro r w) = (Pro r' (changeReps r' r w))
+
+suggestNewRepWeight :: Day -> [DidThis] -> TryThis
+suggestNewRepWeight today = nextWeight' . removeOldCycles today
+  where
+    nextWeight' :: [DidThis] -> TryThis
+    nextWeight' logs = case logs of
+
+      -- Improved over last workout, renormalize at same reps as you were originally told to do.
+      (x:_) | improved x ->
+        {- trace "improved" $ -} TryThis (toldreps x) $ changeReps (toldreps x) (reps x) (weight x)
+
+      -- If you are above 15, regardless of previous workout, readjust down to 15.
+      -- Can happen on first workout, if you severely underestimate your strength.
+      (x:_) | reps x > 15 ->
+        {- trace "above 15" $ -} TryThis 15 $ changeReps 15 (reps x) (weight x)
+
+      -- Stagnant over 3 workouts, renormalize at lower rep count
+      (x:y:z:_) | stagnant x y && stagnant y z ->
+        {- trace "stagnant over 3" $ -} TryThis (toldreps x - 1) $ changeReps (toldreps x - 1) (reps x) (weight x)
+
+      -- Reps have declined and stayed down for two workouts.  Renormalize at lower weight at original rep count.
+      (x:y:_) | declined x && declined y ->
+        {- trace "decline over 2" $ -} TryThis (toldreps x) $ changeReps (toldreps x) (reps x) (weight x)
+
+      -- Stagnant or declined over 2 workouts, just repeat workout
+      (x:y:_) | (stagnant x y || declined x) && (stagnant x y || declined y) -> 
+        {- trace "stagnant or declined over 2" $ -} TryThis (toldreps x) (weight x)
+
+      -- We don't have enough history to determine next workout.  Just repeat what you just did.
+      -- Also, if there are any holes in the above logic, we'll end up here.  Hopefully appropriate.
+      (x:_) -> {- trace "not enough history" $ -} TryThis (reps x) (weight x)
+
+      -- You've never done this before, you figure out what to do, and then tell me how you did for next time.
+      [] -> {- trace "never done before" $ -} TryThis 15 0
+
+    --If the new weight is the same as the old weight, give back the original rep count instead and keep weight the same
+  {-  adjustWeight r' r w = if w' == w
+      then (r,w)
+      else (r',w')
+      where w' = changeReps r' r w -}
+
+    -- If a workout happened 14 days or more ago, it might as well not have happened as far as the
+    -- above rules are concerned.  This happens when you stop doing an exercise for awhile, or fall
+    -- off the wagon entirely for a time.  Might as well start from scratch in that case.
+    removeOldCycles :: Day -> [DidThis] -> [DidThis]
+    removeOldCycles today logs = case logs of
+        [] -> []
+        (x:_) | today > addDays 14 (day x) -> []
+        (x:xs) -> x : removeOldCycles (day x) xs
+
+improved, declined ::  DidThis -> Bool
+improved x = reps x > toldreps x
+declined x = reps x < toldreps x
+stagnant :: DidThis -> DidThis -> Bool
+stagnant x y = toldreps x == toldreps y && reps x <= reps y
+  
+test2 = do
+  today <- fmap (localDay . zonedTimeToLocalTime) getZonedTime
+--  c <- fmap (map (\(day, (Pro r w)) -> (DidThis r w day))) $ liftHistory (Exercise "bsquats" undefined undefined undefined undefined undefined) 20
+  let d = [
+            DidThis 11 11 70 today,
+            DidThis 11 11 70 (addDays (-1) today),
+            DidThis 11 11 70 (addDays (-2) today),
+            DidThis 11 11 72 (addDays (-3) today)]
+  let c= drop 1 d
+  let b= drop 1 c
+  let a = drop 1 b
+  print $ a
+  print $ suggestNewRepWeight today a
+  print $ b
+  print $ suggestNewRepWeight today b
+  print $ c
+  print $ suggestNewRepWeight today c
+  print $ d
+  print $ suggestNewRepWeight today d
+{-
+test = do
+  today <- fmap (localDay . zonedTimeToLocalTime) getZonedTime
+  let a = [DidThis 12 160 (addDays (-5) today)] --, DidThis 8 150 (addDays (-5) today), DidThis 8 150 (addDays (-7) today)]
+  let b = [DidThis 15 160 (addDays (-4) today), DidThis 12 160 (addDays (-5) today)] --, DidThis 8 150 (addDays (-5) today), DidThis 8 150 (addDays (-7) today)]
+  let c = [DidThis 14 170 (addDays (-3) today) ,DidThis 15 160 (addDays (-4) today), DidThis 12 160 (addDays (-5) today)] --, DidThis 8 150 (addDays (-5) today), DidThis 8 150 (addDays (-7) today)]
+  print $ head a
+  print $ suggestNewRepWeight today a
+  print $ head b
+  print $ suggestNewRepWeight today b
+  print $head c
+  print $ suggestNewRepWeight today c
+  return ()
+ -- print $ removeOldCycles today a
+-}
+-- changeReps newreps r w = epleyToNearest5 r w newreps
+changeReps :: Reps -> Reps -> Weight -> Weight
+changeReps newreps r w = nearest 5 $ epley r w newreps
+  where nearest x = (*x) . toRational . round . (/x)
+
+
 {-
  - http://en.wikipedia.org/wiki/One-repetition_maximum#Epley_Formula
  -
@@ -17,92 +132,11 @@ module Weight.Formulas(
  - w'                        = w(r + 30) / (r' + 30)
  -}
 
-import Data.Time
-import Data.List (dropWhileEnd)
-
-import Weight.Types
-
-data TryThis = TryThis Reps Weight deriving Show
-
--- DidThis entries are sorted newest to oldest
-data DidThis  = DidThis {
-  reps :: Reps,
-  weight :: Weight,
-  day :: Day
-} deriving Show
-
-
-adjustProfByReps :: Reps -> Proficiency -> Proficiency
-adjustProfByReps r' (Pro r w) = (Pro r' (changeReps r' r w))
-
-suggestNewRepWeight :: Day -> [DidThis] -> TryThis
-suggestNewRepWeight today = nextWeight' . removeOldCycles today
-  where
-    nextWeight' :: [DidThis] -> TryThis
-    nextWeight' logs = case logs of
-
-      -- Stagnant over 3 workouts, renormalize at lower rep count
-      (x:y:z:_) | reps x <= reps y && reps y <= reps z ->
-        TryThis r' w' where (r',w') = adjustWeight (reps z - 1) (reps z) (weight z)
-
-      -- Reps have declined and stayed down for two workouts.  Renormalize at lower weight at original rep count.
-      (x:y:z:_) | reps x < reps z && reps y < reps z ->
-        TryThis r' w' where (r',w') = adjustWeight (reps z) (reps x) (weight z)
-
-      -- Stagnant over 2 workouts (possibly with decline), repeat original workout
-      (x:y:_)   | reps x <= reps y -> 
-        TryThis (reps y) (weight y)
-
-      -- Improved over last workout, renormalize at same reps.  Go back to find reps you started with at this weight.
-      (x:y:xs)   | reps x > reps lastAtXWeight ->
-        TryThis r' w'
-          where
-            lastAtXWeight = last (dropWhileEnd (\n -> weight n /= weight x) (y:xs))
-            (r',w') = adjustWeight (reps lastAtXWeight) (reps x) (weight x)
-
-      -- If you are above 15, regardless of previous workout, readjust down to 15.
-      -- Can happen on first workout, if you severely underestimate your strength.
-      (x:_)    | reps x > 15 ->
-        TryThis 15 $ changeReps 15 (reps x) (weight x)
-
-      -- We don't have enough history to determine next workout.  Just repeat what you just did.
-      -- Also, if there are any holes in the above logic, we'll end up here.  Hopefully appropriate.
-      (x:_) -> TryThis (reps x) (weight x)
-
-      -- You've never done this before, you figure out what to do, and then tell me how you did for next time.
-      [] -> TryThis 15 0
-
-    --If the new weight is the same as the old weight, give back the original rep count instead and keep weight the same
-    adjustWeight r' r w = if w' == w
-      then (r,w)
-      else (r',w')
-      where w' = changeReps r' r w
-
-    -- If a workout happened 14 days or more ago, it might as well not have happened as far as the
-    -- above rules are concerned.  This happens when you stop doing an exercise for awhile, or fall
-    -- off the wagon entirely for a time.  Might as well start from scratch in that case.
-    removeOldCycles :: Day -> [DidThis] -> [DidThis]
-    removeOldCycles today logs = case logs of
-        [] -> []
-        (x:_) | today > addDays 14 (day x) -> []
-        (x:xs) -> x : removeOldCycles (day x) xs
-
-{-
-test = do
-  today <- fmap (localDay . zonedTimeToLocalTime) getZonedTime
-  let a = [DidThis 20 150 (addDays (-3) today)] -- ,DidThis 6 150 (addDays (-4) today)] --, DidThis 8 150 (addDays (-5) today), DidThis 8 150 (addDays (-7) today)]
-  print $ a
-  print $ suggestNewRepWeight today a
-  return ()
- -- print $ removeOldCycles today a
--}
-
--- changeReps newreps r w = epleyToNearest5 r w newreps
-changeReps :: Reps -> Reps -> Weight -> Weight
-changeReps newreps r w = nearest 5 $ epley r w newreps
-  where nearest x = (*x) . toRational . round . (/x)
 
 epley :: Reps -> Weight -> Reps -> Weight
 epley r w r' = w * ((toRational r) + 30) / ((toRational r') + 30)
+
+epleyCompare :: Reps -> Weight -> Reps -> Weight -> Ordering
+epleyCompare r1 w1 r2 w2 = compare (epley r1 w1 1) (epley r2 w2 1)
 
 --epleyToNearestPoint5 = epley $ (/2) . fromIntegral . round . (*2)
