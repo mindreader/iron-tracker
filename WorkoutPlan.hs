1b204719552cc964a1c036256eed13557e7018e4
David McBride <dmcbride@westipc.com>
2013-04-22 14:50:50 -0400

    Filling out more of workout plan generator, starting with bodyweight.

--- WorkoutPlan.hs
+++ WorkoutPlan.hs
@@ -1,6 +1,9 @@
-{-# LANGUAGE OverloadedStrings, TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings, TemplateHaskell, ScopedTypeVariables #-}
 module WorkoutPlan where
 
+
+import Control.Monad (foldM)
+import Control.Applicative
 
 import Data.Time
 
@@ -40,17 +43,33 @@ makeLenses ''WorkoutStep
 
 plan :: WeightState -> CycleLength -> IO WorkoutPlan
 plan ws cycle = do
-  currentWorkout <- fmap L.sort $ dbFilterCurrentWorkout $  ws ^. exercises . to M.elems
+  currentWorkout <- L.sort <$> dbFilterCurrentWorkout (ws ^. exercises . to M.elems)
   plan' currentWorkout cycle (flip pastHistory 5) undefined
 
 
-prop_plan :: Monad m => WeightState -> m WorkoutPlan
+
+plan' :: forall m. (Functor m, Monad m) => [Exercise] -> CycleLength -> HistFunc m -> SuggestionFunc -> m WorkoutPlan
+plan' exers wCycle histf suggest = Plan . L.reverse <$> foldM nextStep [] exers
+  where
+
+    nextStep :: [WorkoutStep] -> -- ^ Workout up to this point
+                Exercise ->      -- ^ Exercise for this step
+                m [WorkoutStep]  -- ^ Prepend next step to workout so far.
+
+    nextStep sofarSteps exercise = do
+      hist <- histf exercise
+      case exercise ^. eType of
+        Barbell    -> return (undefined:sofarSteps)
+        Dumbbell   -> return (undefined:sofarSteps)
+        Bodyweight -> do
+          let blah = map (view $ _2 . _2 . pReps) hist :: [Reps]
+          return (undefined:sofarSteps)
+
+
+prop_plan :: (Functor m, Monad m) => WeightState -> m WorkoutPlan
 prop_plan _ = plan' testWorkout 12 historyGetter undefined
   where
-    testWorkout = [Exercise "bsquats" "barbell squats" 2 Dumbbell 1]
+    testWorkout = [Exercise "bsquats" "barbell squats" 2 Barbell 1]
     historyGetter x = return []
 
 
-plan' :: Monad m => [Exercise] -> CycleLength -> HistFunc m -> SuggestionFunc -> m WorkoutPlan
-plan' exers wCycle histf suggest = do
-  undefined
