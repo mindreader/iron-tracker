0629d363018ba90ec2504436441d168e96b609c7
David McBride <dmcbride@westipc.com>
2013-04-23 16:11:01 -0400

    New workout mode! Better than the old one! (untested).

--- Weight.hs
+++ Weight.hs
@@ -4,14 +4,14 @@ module Weight(
 ) where
 
 import Control.Lens
-import Data.Time (localDay,zonedTimeToLocalTime,getZonedTime,Day)
+import Data.Time (Day)
 
 import qualified Data.Text as T
 import qualified Data.Map as M
 import qualified Data.List as L
 
 import Data.Default
-import Safe
+import Safe (headMay)
 
 
 import Control.Monad.State
@@ -23,9 +23,12 @@ import Weight.Types
 import Weight.Log
 import qualified Weight.PlateCalc as PC
 import Weight.PlateOrder as PO
+import WorkoutPlan
 
 import Menu
 import IO
+
+import Control.Exception (bracket_)
 
 data WeightMenuCommand = MWWorkoutMode | MWWorkoutStatus | MWAdjustWorkoutReps | MWUpdate | MWInclude | MWDisInclude deriving (Eq, Ord)
 
@@ -34,8 +37,7 @@ newtype App a = App (StateT AppState IO a)
 
 data AppState = AS {
   _weightState :: WeightState,
-  _wCycle :: Integer,
-  _today :: Day
+  _wCycle :: Integer
 }
 
 makeLenses ''AppState
@@ -45,9 +47,8 @@ runWeightRoutine = runApp mainLoop
 
 runApp :: App a -> IO a
 runApp (App s) = do
-  today <- fmap (localDay . zonedTimeToLocalTime) getZonedTime
   ws <- loadWeightConfig
-  evalStateT s (AS ws 12 today)
+  evalStateT s (AS ws 12)
 
 
 mainLoop :: App ()
@@ -78,47 +79,35 @@ mainLoop = do
       (MWDisInclude,        "Remove exercise from workout")]
 
 
--- On `Day` you were told to do `Reps` but you were able to do (Proficiency Reps Weight).
+-- !On `Day` you were told to do `Reps` but you were able to do (Proficiency Reps Weight).
 type History = [(Day, (Reps, Proficiency))]
-
 
 workoutMode :: App ()
 workoutMode = do
-  workout <- currentWorkout
+  ws <- use weightState
   wCycle <- use wCycle
-  today <- use today
-  (exers, histories, plates, tryThis) <- fmap L.unzip4 $ mapM (fetchPlateConfig wCycle today) workout :: App ([Exercise], [History], [[PO.Plate]], [TryThis])
-  workoutMode' [] (L.zip3 exers histories tryThis) plates
+  (Plan steps) <- liftIO $ workoutPlan ws wCycle
+  forM_ steps $ \step -> do
+    beforeStep step
+    workoutStep step
+    afterStep step
   liftIO $ printf "Workout complete.\n"
   where
-      
-    fetchPlateConfig :: Integer -> Day -> Exercise -> App (Exercise, History, [PO.Plate], TryThis)
-    fetchPlateConfig wCycle today exer = do
-      history <- pastHistory exer 5
-      let tryThis@(TryThis _ tw) = suggestNewRepWeight wCycle today $ map (\(date,(attemptedReps,Pro r w)) -> DidThis attemptedReps r w date) history
-      return $ (exer, history, case PC.plateCalc PC.Barbell tw of PC.Plates ps -> plateOrder2Calc ps, tryThis)
+    workoutStep (BarbellExercise exer reps weight plates) = do
+      liftIO $ printf "You must do %s.\n" (formatRepsWeight reps weight (Just $ PO.displayPlates plates))
+    workoutStep (DumbbellExercise exer reps weight) = do
+      liftIO $ printf "You must do %s.\n" (formatRepsWeight reps weight Nothing)
+    workoutStep (BodyWeightExercise exer reps) = do
+      liftIO $ printf "You must do %d reps.\n" reps
 
-    -- TODO move to general purpose plate library for cleaner interface.
-    plateOrder2Calc :: [PC.Plate] -> [PO.Plate]
-    plateOrder2Calc (PC.P45  n :ps) = replicate (n `div` 2) PO.P45  ++ plateOrder2Calc ps
-    plateOrder2Calc (PC.P25  n :ps) = replicate (n `div` 2) PO.P25  ++ plateOrder2Calc ps
-    plateOrder2Calc (PC.P10  n :ps) = replicate (n `div` 2) PO.P10  ++ plateOrder2Calc ps
-    plateOrder2Calc (PC.P5   n :ps) = replicate (n `div` 2) PO.P5   ++ plateOrder2Calc ps
-    plateOrder2Calc (PC.P2p5 n :ps) = replicate (n `div` 2) PO.P2p5 ++ plateOrder2Calc ps
-    plateOrder2Calc _ = []
+    beforeStep step = liftIO (pastHistory (_sExercise step) 5) >>= printHistory
+    afterStep step = inputReps >>= (\reps -> logLift (_sExercise step) (_sReps step, Pro reps (_sWeight step)))
 
-    workoutMode' :: [PO.Plate] -> [(Exercise, History, TryThis)] -> [[PO.Plate]] -> App ()
-    workoutMode' _ [] _ = return ()
-    workoutMode' lastPlateOrder ((exer,history,(TryThis tr tw)):xs) plates@(_:ps) = do
-      liftIO $ printf "\n%s\n" (exer ^. eName)
-      printHistory history
-      let thisPlateOrder = optimalPlateOrder lastPlateOrder $ plates
-      liftIO $ case exer ^. eType of
-        Barbell -> printf "You must do %s.\n" (formatRepsWeight tr tw (Just $ PO.displayPlates thisPlateOrder))
-        Bodyweight -> printf "You must do whatever.\n"
-        Dumbbell -> printf "You must do however many at %d" (999::Int)
-      inputReps >>= (\reps -> logLift exer (tr, Pro reps tw))
-      workoutMode' thisPlateOrder xs ps
+    formatRepsWeight :: Reps -> Weight -> (Maybe String) -> String
+    -- TODO this really should suggest the last weight we did.  If we did it over two weeks ago, it doesn't have the info at this point in code.
+    formatRepsWeight reps 0.0 _ = printf "%d" reps
+    formatRepsWeight reps weight (Just plates) = printf "%d@%s (%s)" reps (rTrimZeros $ show $ (fromRational weight :: Double)) plates
+    formatRepsWeight reps weight Nothing = printf "%d@%s (%s)" reps (rTrimZeros $ show $ (fromRational weight :: Double)) (PC.displayPlateCalc PC.Barbell weight)
 
     printHistory :: History -> App ()
     printHistory history = do
@@ -126,11 +115,6 @@ workoutMode = do
       where
         printHistory' (day, (attempted,pro)) = liftIO $ printf " %s : tried %d did %s\n" (show day) attempted (formatRepsWeight (pro ^. pReps) (pro ^. pWeight) Nothing)
 
-    formatRepsWeight :: Reps -> Weight -> (Maybe String) -> String
-    -- TODO this really should suggest the last weight we did.  If we did it over two weeks ago, it doesn't have the info at this point in code.
-    formatRepsWeight reps 0.0 _ = printf "%d" reps
-    formatRepsWeight reps weight (Just plates) = printf "%d@%s (%s)" reps (rTrimZeros $ show $ (fromRational weight :: Double)) plates
-    formatRepsWeight reps weight Nothing = printf "%d@%s (%s)" reps (rTrimZeros $ show $ (fromRational weight :: Double)) (PC.displayPlateCalc PC.Barbell weight)
 
 inputReps :: App Reps
 inputReps = liftIO $ prompt "New reps:"
