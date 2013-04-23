f485d2f30f6e8f002d26f34db86036d7be657180
David McBride <dmcbride@westipc.com>
2013-04-23 15:17:14 -0400

    Adding some haddock friendly documentation.

--- Weight/Types.hs
+++ Weight/Types.hs
@@ -8,26 +8,27 @@ import qualified Data.Map as M
 import Control.Applicative
 import Data.Default
 
+-- !A collection of exercises that are known to exist.  A person will usually have selected only a subset of these at any given time.
 data WeightState = WS {
-  _exercises :: M.Map T.Text Exercise -- indexed by their id
+  _exercises :: M.Map T.Text Exercise -- ^ indexed by their id
 } deriving (Show)
 
 instance Default WeightState where
   def = WS M.empty
 
 
--- How many days should go by without doing an exercise
--- before you should just start over again at 15 reps.
+-- !How many days should go by without doing an exercise
+--  before you should just start over again at 15 reps.
 type CycleLength = Integer
 
 data ExerciseType = Bodyweight | Dumbbell | Barbell deriving (Eq, Show)
 
 data Exercise = Exercise {
-  _eExerciseId :: T.Text,  -- "bsquats"
-  _eName       :: T.Text,  -- "Barbell Squats"
-  _eMinReps    :: Int,     -- minimum I am willing to do
-  _eType       :: ExerciseType, -- barbell, dumbbell, or bodyweight
-  _eRank       :: Float    -- Determines where exercise will appear in workout
+  _eExerciseId :: T.Text,       -- ^ "bsquats"
+  _eName       :: T.Text,       -- ^ "Barbell Squats"
+  _eMinReps    :: Int,          -- ^ minimum I am willing to do
+  _eType       :: ExerciseType,
+  _eRank       :: Float         -- ^ Determines where exercise will appear in workout (TODO get rid of this hack)
 } deriving (Eq, Show)
 
 
